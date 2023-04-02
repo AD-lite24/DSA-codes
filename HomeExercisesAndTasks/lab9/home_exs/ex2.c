a6a3590777564f2afdc2b24aa5f7b159d14798a9
Aditya Dandwate <adityadandwate24@gmail.com>
2023-04-02 15:53:35 +0530

    added some bst tasks;

---
+++ HomeExercisesAndTasks/lab9/home_exs/ex2.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "bst.h"
+
+void printAtCurrLevel(Node *root, int level)
+{
+    if (root == NULL)
+        return;
+
+    if (level == 1)
+        printf(" %d ", root->value);
+
+    else
+    {
+        printAtCurrLevel(root->left, level - 1);
+        printAtCurrLevel(root->right, level - 1);
+    }
+}
+
+void printLevels(Node *node)
+{
+    int h = height(node);
+
+    for (int i = h; i > 0; i--)          
+        printAtCurrLevel(node, i);
+}
+
+void reverseOrderTraversal(Node *node)
+{
+    printLevels(node);
+}
+
+int main()
+{
+    BST *bst = new_bst();
+    insert(bst, 6);
+    insert(bst, 5);
+    insert(bst, 2);
+    insert(bst, 5);
+    insert(bst, 7);
+    insert(bst, 8);
+
+    printf("Traversing level order \n");
+    reverseOrderTraversal(bst->root);
+
+    return 0;
+}
--- HomeExercisesAndTasks/lab9/tasks/task3/stack_array.c
+++ HomeExercisesAndTasks/lab9/tasks/task3/stack_array.c
@@ -1,23 +1,25 @@
-#include "element.h"
 #include "stack.h"
 #include <stdlib.h>
 #include <stdbool.h>
+#include "bst.h"
 
-#define STACK_SIZE 1000
+#define STACK_SIZE 100
 struct Stack
 {
-int top;
-Element data[STACK_SIZE];
+    int top;
+    Node* data[STACK_SIZE];
 };
 
 Stack *newStack()
 {
     Stack *s = (Stack *)malloc(sizeof(Stack));
+    for (int i = 0; i < STACK_SIZE; i++)
+        s->data[i] = malloc(sizeof(Node));
     if(s != NULL)
         s->top = -1;
     return s;
 }
-bool push(Stack *s, Element e)
+bool push(Stack *s, Node* e)
 {
     if(s->top == STACK_SIZE - 1)
         return false;
@@ -32,13 +34,13 @@ bool pop(Stack *s){
     return true;
 }
 
-Element *top(Stack *s){
+Node *top(Stack *s){
     
     if (s->top == -1){
         return NULL;
     }
 
-    return &(s->data[s->top]);
+    return s->data[s->top];
 }
 
 bool isEmpty(Stack *s){
